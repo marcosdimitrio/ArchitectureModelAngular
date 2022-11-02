be8e787f484d94e20962177e79770717bf95a636
Marcos Dimitrio <marcos@dimitrio.com>
2022-11-02 20:27:14 -0300

    Extract the SieveService dependency

---
+++ src/app/core/sieve/sieve.service.ts
@@ -0,0 +1,37 @@
+import { HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { PageRequest, Sort } from "../pagination-datasource/page";
+
+@Injectable({ providedIn: 'root' })
+export class SieveService {
+
+    getHttpParamsForSieve<T>(request: PageRequest<T>, filters: string): HttpParams {
+        const sorts = this.getSort(request.sort);
+
+        return new HttpParams()
+            .set('page', request.page)
+            .set('pageSize', request.size)
+            .set('sorts', sorts)
+            .set('filters', filters);
+    }
+
+    escapeForSieve(search: string): string {
+        if (search == "null") return "\\null";
+
+        let result = search;
+
+        result = result.replace(",", "\\,");
+        result = result.replace("|", "\\|");
+
+        return result;
+    }
+
+    private getSort<T>(sort: Sort<T> | undefined): string {
+        if (sort == undefined || sort.property == undefined) return "";
+
+        const sign = sort.order == "desc" ? "-" : "";
+
+        return sign + sort.property.toString();
+    }
+
+}
--- src/app/students/shared/students.service.ts
+++ src/app/students/shared/students.service.ts
@@ -6,54 +6,29 @@ import { isNullOrWhiteSpace } from "../../core/utils/string-utils";
 import { Student } from "./models/student.model";
 import { StudentQuery } from "../../students/shared/student-query";
 import { PageRequest, Page, Sort } from "../../core/pagination-datasource/page";
+import { SieveService } from "../../core/sieve/sieve.service";
 
 @Injectable({ providedIn: 'root' })
 export class StudentsService {
 
-    constructor(private http: HttpClient) { }
+    constructor(
+        private http: HttpClient,
+        private sieveService: SieveService,
+    ) { }
 
     get(request: PageRequest<Student>, query: StudentQuery): Observable<Page<Student>> {
-        const httpParams = this.getHttpParamsForSieve(request, query);
+        const filters = this.getFilters(query);
+        const httpParams = this.sieveService.getHttpParamsForSieve(request, filters);
 
         return this.http.get<Page<Student>>('/api/students', {
             params: httpParams
         });
     }
 
-    private getHttpParamsForSieve(request: PageRequest<Student>, query: StudentQuery): HttpParams {
-        const sorts = this.getSort(request.sort);
-        const filters = this.getFilters(query);
-
-        return new HttpParams()
-            .set('page', request.page)
-            .set('pageSize', request.size)
-            .set('sorts', sorts)
-            .set('filters', filters);
-    }
-
     private getFilters(query: StudentQuery): string {
         if (isNullOrWhiteSpace(query.search)) return "";
 
-        return "(name|email)@=" + this.escape(query.search);
-    }
-
-    private escape(search: string): string {
-        if (search == "null") return "\\null";
-
-        let result = search;
-
-        result = result.replace(",", "\\,");
-        result = result.replace("|", "\\|");
-
-        return result;
-    }
-
-    private getSort(sort: Sort<Student> | undefined): string {
-        if (sort == undefined || sort.property == undefined) return "";
-
-        const sign = sort.order == "desc" ? "-" : "";
-
-        return sign + sort.property;
+        return "(name|email)@=" + this.sieveService.escapeForSieve(query.search);
     }
 
 }
